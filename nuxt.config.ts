7a385a9194394531c4adeb7c0d8d8f10b7cacd2a
Marvin Parada <marvin@loaded.gg>
2025-05-07 12:26:36 -0700

    Add shadcn-vue.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,25 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-  typescript: {
-    typeCheck: false
+  modules: ['shadcn-nuxt'],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
   }
 })
