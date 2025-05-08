43930be89df137ce98bfa79dfd2a61581b1bbc7e
Marvin Parada <marvin@loaded.gg>
2025-05-07 17:17:17 -0700

    Add Highcharts.

---
+++ components/custom/Highcharts.vue
@@ -0,0 +1,14 @@
+<script lang='ts' setup>
+import Highcharts from 'highcharts'
+import { createHighcharts } from 'vue-highcharts'
+
+// Highcharts docs available at: https://api.highcharts.com/highcharts/
+
+defineProps<{
+  options: Highcharts.Options
+}>()
+</script>
+
+<template>
+  <component :is="createHighcharts('Highcharts', Highcharts)" :options="options" />
+</template>
---
+++ types/vue-highcharts.d.ts
@@ -0,0 +1,2 @@
+// Fixes: "Could not find a declaration file for module 'vue-highcharts'." error when using `vue-highcharts` since author does not provide `d.ts` file.
+declare module 'vue-highcharts'
